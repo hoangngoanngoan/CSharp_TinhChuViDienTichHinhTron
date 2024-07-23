1f8128f2db558afb6c06a091c4b65e757e25604f
hoangngoanngoan <ldh2381995@gmail.com>
2024-07-23 20:04:19 +0700

    Sửa Code

--- CSharp_TinhChuViDienTichHinhTron/HinhTron.cs
+++ CSharp_TinhChuViDienTichHinhTron/HinhTron.cs
@@ -10,14 +10,36 @@ namespace CSharp_TinhChuViDienTichHinhTron
         }
 
         private double bk;
-        private void getinfo()
-        {
+        const double pi = 3.14;
 
+        
+        private void GetInfo()
+        {
+            bk = Convert.ToDouble(txtBK.Text);
         }
 
+        private bool CheckInfo()
+        {
+            try
+            {
+                GetInfo();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Phải nhập số","Thông Báo",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return false;
+            }
+        }
+        private void TinhCV()
+        {
+            GetInfo();
+            labKQ.Text = "Chu vi: " + bk * pi; 
+        }
         private void butTinh_Click(object sender, EventArgs e)
         {
-
+            if (CheckInfo())  TinhCV();
+            
         }
     }
 }
